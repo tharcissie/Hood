f7822fe3071e0f5944f4acc3505cf7ced7b03942
tharcissie <tharcissieidufashe@gmail.com>
2021-01-24 20:11:51 +0200

    added a template

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from * import views
 
 urlpatterns=[
-    path('', home.views, name=home)
+    path('', dashboard.views.views, name=home)
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from app.forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
+def Dashboard(request):
+    return render(request, 'base.html',{})
+
+
